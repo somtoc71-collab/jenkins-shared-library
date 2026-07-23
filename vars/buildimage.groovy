585a2720791afcda9fe491fcb7d8f06b8ce02e99
somto collins <somto>
2026-07-23 23:47:47 +0100

    initial commit

---
+++ vars/buildimage.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo 'building the image ..'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
+    sh 'docker build -t bamzy14/my-repo:jma-2.0 .'
+    sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+    sh 'docker push bamzy14/my-repo:jma-2.0'
+}
