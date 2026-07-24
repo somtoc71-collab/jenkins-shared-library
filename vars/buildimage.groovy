a83ed3d78107d2db7708a9daf64c96a05862d33b
somtoc71-collab <somtoc71@gmail.com>
2026-07-24 12:05:54 +0100

    Fix syntax for withCredentials block in buildimage.groovy

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -2,7 +2,7 @@
 
 def call() {
     echo 'building the image ..'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
     sh 'docker build -t bamzy14/my-repo:jma-2.0 .'
     sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
     sh 'docker push bamzy14/my-repo:jma-2.0'
